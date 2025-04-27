db0e819810fd257f35978a7ba1cc4e593d32c06e
gadgieOps <gadgieOps@gmail.com>
2025-04-27 11:20:18 +0100

    provider

--- terraform/vault/ca/provider.tf
+++ terraform/vault/ca/provider.tf
@@ -8,5 +8,12 @@ terraform {
 }
 
 provider "vault" {
-  # Configuration options
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id   = var.TF_VAR_role_id
+      secret_id = var.TF_VAR_secret_id
+    }
+  }
 }
---
+++ terraform/vault/ca/variables.tf
@@ -0,0 +1,10 @@
+variable TF_VAR_role_id {
+    description = "Role ID for AppRole authentication"
+    type        = string
+    default     = ""
+}
+variable TF_VAR_secret_id {
+    description = "Secret ID for AppRole authentication"
+    type        = string
+    default     = ""
+}
