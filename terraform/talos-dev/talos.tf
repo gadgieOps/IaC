b63b780ffbb750a549d6f34e24460d641f48ef7c
gadgieOps <gadgieOps@gmail.com>
2026-03-03 22:57:07 +0000

    Add EC2 instances and associated networking

--- terraform/talos-dev/talos.tf
+++ terraform/talos-dev/talos.tf
@@ -22,3 +22,32 @@ data "aws_ami" "talos" {
 output "talos_ami_id" {
     value = data.aws_ami.talos.id
 }
+
+resource "aws_instance" "talos-dev" {
+    count                       = length(aws_subnet.talos-dev-subnets)
+    ami                         = data.aws_ami.talos.id
+    instance_type               = var.talos_instance_type
+    subnet_id                   = aws_subnet.talos-dev-subnets[count.index].id
+    vpc_security_group_ids      = [aws_security_group.talos-dev-cp-sg.id]
+    associate_public_ip_address = true
+
+    tags = {
+        Name = "talos-dev-${count.index + 1}"
+    }
+}
+
+resource "aws_lb_target_group_attachment" "talos-dev-nlb-tg-attachment" {
+    count            = length(aws_instance.talos-dev)
+    target_group_arn = aws_lb_target_group.talos-dev-nlb-tg.arn
+    target_id        = aws_instance.talos-dev[count.index].id
+    port             = 6443
+}
+
+resource "aws_route53_record" "talos-dev-instances" {
+    count   = min(3, length(aws_instance.talos-dev))
+    zone_id = data.aws_route53_zone.selected.zone_id
+    name    = "talos-dev-${count.index + 1}.${var.hosted_zone}"
+    type    = "A"
+    ttl     = 300
+    records = [aws_instance.talos-dev[count.index].public_ip]
+}
--- terraform/talos-dev/variables.tf
+++ terraform/talos-dev/variables.tf
@@ -2,3 +2,9 @@ variable "hosted_zone" {
   description = "The public domain for the environment"
   type        = string
 }
+
+variable "talos_instance_type" {
+  description = "EC2 instance type for Talos nodes"
+  type        = string
+  default     = "t3.medium"
+}
